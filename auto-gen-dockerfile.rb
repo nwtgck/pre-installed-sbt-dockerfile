a77d5ff97cfa13afea62919ce6a06b8457016e27
Ryo Ota <nwtgck@gmail.com>
2017-12-01 08:56:28 +0900

    [change/remove] Change auto-gen-dockerfile.rb to support >= 0.13.6 and Remove unsupported Dockerfile

--- auto-gen-dockerfile.rb
+++ auto-gen-dockerfile.rb
@@ -83,9 +83,10 @@ SBT_VERSIONS = %w{
     v0.10.1
     v0.10.0
 }
-.map{|v| v[1..-1]} # NOTE: Remove 'v'
-.sort_by{|v| Gem::Version.new(v)} # Sort by natual version
-.reverse
+  .map{|v| v[1..-1]} # NOTE: Remove 'v'
+  .sort_by{|v| Gem::Version.new(v)} # Sort by natual version
+  .select{|v| Gem::Version.new(v) >= Gem::Version.new("0.13.6")} # Support >= 0.13.6
+  .reverse
 
 
 SBT_VERSIONS.each{|sbt_version|
